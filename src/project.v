3dba7f1eeb11d6ccfdbbb8b2b1237e14269dd559
SRIRAM NARAYAN KOUSHIK CHITRAPU <koushikcsn@gmail.com>
2024-10-10 12:30:26 -0400

    Reverted Inouts
    Removed the VPWR, VGND that I just added

--- src/project.v
+++ src/project.v
@@ -5,8 +5,6 @@
 `default_nettype none
 
 module tt_um_Richard28277 (
-    inout wire VPWR,
-    inout wire VGND,
     input  wire [7:0] ui_in,    // Dedicated inputs (a and b)
     output wire [7:0] uo_out,   // Dedicated outputs (result)
     input  wire [7:0] uio_in,   // IOs: Input path (opcode)
