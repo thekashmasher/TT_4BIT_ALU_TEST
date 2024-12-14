b2e1504d9e0e854c8e4cc5e421974e8f9e7d0d2d
thekashmasher <120293801+thekashmasher@users.noreply.github.com>
2024-12-14 00:20:20 -0500

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,19 +1,21 @@
 `default_nettype none
 `timescale 1ns / 1ps
 
-module tb();
+/* This testbench just instantiates the module and makes some convenient wires
+   that can be driven / tested by the cocotb test.py.
+*/
+module tb ();
 
-  // Dump the signals to a VCD file
+  // Dump the signals to a VCD file. You can view it with gtkwave.
   initial begin
     $dumpfile("tb.vcd");
     $dumpvars(0, tb);
+    #10;
   end
 
-  // Clock and reset
+  // Wire up the inputs and outputs:
   reg clk;
   reg rst_n;
-
-  // I/O signals
   reg ena;
   reg [7:0] ui_in;
   reg [7:0] uio_in;
@@ -21,36 +23,27 @@ module tb();
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
 
-  // Power signals for gate-level simulations
-  supply1 VPWR;
-  supply0 VGND;
 
-  // Instantiate the DUT
+   supply1 VPWR;
+   supply0 VGND;
+   
+  // Replace tt_um_example with your module name:
   tt_um_Richard28277 user_project (
-    `ifdef GL_TEST
-      .VPWR(VPWR),
-      .VGND(VGND),
-    `endif
-      .ui_in  (ui_in),
-      .uo_out (uo_out),
-      .uio_in (uio_in),
-      .uio_out(uio_out),
-      .uio_oe (uio_oe),
-      .ena    (ena),
-      .clk    (clk),
-      .rst_n  (rst_n)
+
+      // Include power ports for the Gate Level test:
+   `ifdef GL_TEST
+     .VPWR(VPWR),
+     .VGND(VGND),
+   `endif
+
+      .ui_in  (ui_in),    // Dedicated inputs
+      .uo_out (uo_out),   // Dedicated outputs
+      .uio_in (uio_in),   // IOs: Input path
+      .uio_out(uio_out),  // IOs: Output path
+      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
+      .ena    (ena),      // enable - goes high when design is selected
+      .clk    (clk),      // clock
+      .rst_n  (rst_n)     // not reset
   );
-
-  // Clock generation
-  initial begin
-    clk = 0;
-    forever #5 clk = ~clk;  // 10 ns clock period
-  end
-
-  // Reset initialization
-  initial begin
-    rst_n = 0;  // Start in reset
-    #50 rst_n = 1;  // Release reset after 50 ns
-  end
 
 endmodule
